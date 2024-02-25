47a05c3c96d51a30dfc9a03b3d833a6f8249bfa5
Timi2424 <tymonbieganski@wp.pl>
2024-02-25 21:40:23 +0100

    Todo App

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { FC, useContext, useEffect } from 'react';
+import { Todo } from './types/Todo';
+import { getTodos } from './api/todos';
+import { Footer } from './components/Footer';
+import { FilterBy } from './types/Filter';
+import { TodoList } from './components/TodoList';
+import { filterTodo } from './components/TodoFilter';
+import { UserWarning } from './UserWarning';
+import { Header } from './components/Header';
+import { ErrorType } from './types/Errors';
+import { Error } from './components/Error';
+import { USER_ID } from './utils/userId';
+import { AppContext } from './ContextProvider/AppContext';
+
+
+
+
+export const App: FC = () => {
+  const {
+    todos,
+    setTodos,
+    filterBy,
+    setErrorMessage,
+  } = useContext(AppContext);
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setErrorMessage(ErrorType.LOAD));
+  }, [setErrorMessage, setTodos]);
+
+  const selectFilterTodo = (
+    todosFromServer: Todo[],
+    optionByFilter: FilterBy,
+  ) => (
+    filterTodo(todosFromServer, optionByFilter));
+
+  const preparedTodos = selectFilterTodo(todos, filterBy);
+
+  const isEveryTodosCompleted = preparedTodos.every(
+    todo => todo.completed,
+  );
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header isEveryTodosCompleted={isEveryTodosCompleted} />
+
+        <TodoList todos={preparedTodos} />
+
+        {todos.length > 0 && (
+          <Footer />
+        )}
+
+      </div>
+
+      <Error />
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { AppProvider } from './ContextProvider/AppContext';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/index.scss';
+import { App } from './App';
+
+
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(
+    <AppProvider>
+      <App />
+    </AppProvider>,
+  );
